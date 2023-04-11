a2aed58d3bcd3607cf6d16c1e7bc5014463bc6b0
Fernanda-Franco <fernandaf.vet@gmail.com>
2023-04-11 15:22:44 -0300

    exercicios node

--- 5_hello_world_express/index.js
+++ 5_hello_world_express/index.js
@@ -5,48 +5,148 @@
 
 const express = require("express");
 const fs = require("fs");
+const CPF = require("cpf");
 
-//Define uma aplicação backend em Express
-//Recursos pré configurados
+// Define uma aplicação backend em Express
+// Recursos pré-configurados
 const app = express();
 
-//Define um roteamento
-//Manipulador de rota
+// Define um roteamento
+// Manipulador de rota
 app.get("/", (requisicao, resposta) => {
-    resposta.send("Batata!");
+  resposta.send("<h2>Batata!!!!</h2>");
 });
 
-//req = requisição do cliente
-//res = resposta do servidor
+// req = requisicao do cliente
+// res = resposta do servidor
 app.get("/teste", (req, res) => {
-    //manipulador de rota
-    //req = objeto com dados do cliente/solicitante
-    //res = objeto com dados p/ resposta do servidor 
-    res.send("<p>O que deseja amigo?</p>");
+  // manipulador de rota
+  // req = objeto com dados do cliente/solicitante
+  // res = objeto com dados p/ resposta do servidor
+  res.send("<p>O que deseja amigo?</p>");
 });
-
-/** Exercício I: Crie dois arquivos html: inicio.html e
- * ajuda.html. Defina uma rota GET /inicio que lê o arquivo
- * inicio.html e responde com seu conteúdo. Defina outra
- * rota /ajuda que lê o arquivo ajuda.html e responde com
- * seu conteúdo. Dentro do arquivo inicio.html, deve haver
- * um link para a página de ajuda.**/
 
 app.get("/inicio", (req, res) => {
-    const arquivo = fs.readFileSync("./inicio.html");
-    res.send(arquivo.toString());
+  const arquivo = fs.readFileSync("./inicio.html");
+  res.send(arquivo.toString());
 });
 
 app.get("/ajuda", (req, res) => {
-    const arquivo = fs.readFileSync("./ajuda.html");
-    res.send(arquivo.toString());
-})
+  const arquivo = fs.readFileSync("./ajuda.html");
+  res.send(arquivo.toString());
+});
 
-//Parametro de caminho/rota
+// Parâmetro de caminho/rota
 app.get("/funcionarios/:cpf", (req, res) => {
-    console.log(req.params);
-    res.send("Servidor rodando em http://localhost:3000/");
-})
+  // req.params = guarda todos os parametros de rota
+  // const cpf = req.params.cpf;
+  const { cpf } = req.params;
 
-//Inicializa a escuta de requisição do servidor
-app.listen(3000);
+  if (CPF.isValid(cpf)) {
+    // Por padrão status é 200
+    res.send("O cpf é válido!");
+  } else {
+    // 400 = quando o cliente manda informação
+    // inválida
+    res.status(400).send("O cpf é inválido!");
+  }
+});
+
+app.get("/pessoas/:nome/:empresa", (req, res) => {
+  // const nome = req.params.nome;
+  // const empresa = req.params.empresa;
+  const { nome, empresa } = req.params;
+  res.send(`${nome} e ${empresa}`);
+});
+
+app.get("/imc/:peso/:altura", (req, res) => {
+  const peso = Number(req.params.peso);
+  const altura = Number(req.params.altura);
+  const imc = peso / altura ** 2;
+
+  res.send(`<p>IMC: ${imc.toFixed(2)}</p>`);
+});
+
+app.get("/cpfs/:numero", (req, res) => {
+  const numero = Number(req.params.numero);
+
+  for (let i = 0; i < numero; i++) {
+    res.write(`<p>${CPF.generate()}</p>`);
+  }
+  res.end();
+});
+
+// Query // consulta
+app.get("/youtube", (req, res) => {
+  // const canal = req.query.canal;
+  const { canal, video, r } = req.query;
+  res.send(`Canal: ${canal}. Vídeo: ${video}. Res: ${r}`);
+});
+
+// Exercício I: Crie uma rota que receba um parâmetro
+// query `nome` e retorne uma mensagem de boas-vindas
+// personalizada. Se `nome` não for fornecido, exiba uma
+// mensagem de erro.
+
+app.get("/nome", (req, res) => {
+  const { nome } = req.query;
+
+  if (nome) {
+    // truthy values
+    res.send(`Olá, ${nome}!`);
+  } else {
+    res.status(400).send("Envie o nome corretamente!");
+  }
+});
+// Exercício II: Crie uma rota que receba dois 
+//parâmetros query, `num1` e `num2`. Retorne como 
+//resposta a soma dos números (se os dois numeros 
+//não forem fornecidos mostrar uma mensagem de erro).
+
+app.get("/soma", (req, res) => {
+  const { num1, num2 } = req.query;
+
+  // Checa se os valores foram fornecidos
+  if (num1 !== undefined && num2 !== undefined) {
+    const soma = Number(num1) + Number(num2);
+    res.send(`A soma é: ${soma}`);
+  } else {
+    res.status(400).send("Forneça dois números válidos");
+  }
+});
+
+// Exercício III: Crie uma rota que receba um parâmetro
+// query, `lang` e exiba uma mensagem de boas vindas no 
+//idioma (português ou inglês). Caso a linguagem não seja
+// fornecida ou suportada exiba uma mensagem de erro.
+
+app.get("/boasvindas", (req, res) => {
+  const { lang } = req.query;
+
+  if (lang === "pt") {
+    res.send("Bem-vindo!");
+  } else if (lang === "en") {
+    res.send("Welcome!");
+  } else {
+    res.status(400).send("Idioma não suportado!");
+  }
+});
+
+const usuarios = require("./usuarios");
+
+app.get("/usuarios", (req, res) => {
+    res.json(usuarios);
+});
+
+// Params = mais restrito a url
+// /cpfs/5
+
+// Query = mais flexível, suja mais a url
+// /cpfs?numero=5
+// Mais propósito em filtrar dados
+
+// Inicializa a escuta de requisições do servidor
+app.listen(3000, () => {
+  // roda sempre que o servidor inicia com sucesso
+  console.log("Servidor rodando em http://localhost:3000/");
+});
