4b4e8a00ee11dee89f02244ff93c8a50cb7f8801
Fernanda-Franco <fernandaf.vet@gmail.com>
2023-04-12 09:39:59 -0300

    exercicios node

---
+++ 5_hello_world_express/usuarios.js
@@ -0,0 +1,5 @@
+const usuarios = [
+    {nome: "Fernanda Franco", email: "fernandaf.vet@gmail.com"},
+    {nome: "José Almir", email: "jose.almir@soulcode.com"},
+    {nome: "Gabriel Braga", email: "gabriel.braga@soulcode.com"}
+]
